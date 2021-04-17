ba9f36b44ce373cb918d274c7bbcdbc6b33d1a8d
aladdin731 <aladdin731@hotmail.com>
2021-04-16 22:07:58 -0700

    change back to public

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const db = require('./config/keys').mongoURI;
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/public'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
